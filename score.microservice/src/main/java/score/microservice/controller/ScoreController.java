6600ab0c0a8e4711a1ce652a6b98a49e6da36b7f
saharsahbaa <sahar.sahbaee@gmail.com>
2020-07-11 11:02:52 +0200

    Update Score Service

--- score.microservice/src/main/java/score/microservice/controller/ScoreController.java
+++ score.microservice/src/main/java/score/microservice/controller/ScoreController.java
@@ -1,29 +1,15 @@
 package score.microservice.controller;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.validation.Valid;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.mongodb.core.MongoAction;
-import org.springframework.data.mongodb.core.MongoAdmin;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.core.aggregation.LimitOperation;
-import org.springframework.data.mongodb.core.aggregation.MatchOperation;
-import org.springframework.data.mongodb.core.aggregation.SortOperation;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,8 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.annotation.JsonView;
-import com.mongodb.BasicDBObject;
-import com.mongodb.MongoClient;
 
 import score.microservice.Views;
 import score.microservice.model.Game;
@@ -87,14 +71,14 @@ public class ScoreController {
 			List<Score> score = repo.findAll();
 			return ResponseEntity.ok(score);
 		} catch (Exception e) {
-			return ResponseEntity.status(400).body("There Is A problem!");
+			return ResponseEntity.status(400).body("Bad Request!");
 		}
 	}
 	//UPDATE SCORE
 	@PutMapping
 	public ResponseEntity<?> updateScore(@RequestBody @Valid  Score score){
 		try {
-			String id = score.getPlayer()+"$"+score.getGamecode();
+			//String id = score.getPlayer()+"$"+score.getGamecode();
 			List<Score> sc = repo.findByPlayerOrderByScoreDesc(score.getPlayer());
 			map.put("score", score.getScore());
 			map.put("date", score.getDate());
@@ -103,11 +87,17 @@ public class ScoreController {
 			return ResponseEntity.ok(sc);
 			
 		} catch (Exception e) {
-			return ResponseEntity.status(400).body("RIDIIIII");
+			return ResponseEntity.status(400).body("Bad Request!");
 		}
 		
 		//List<Score> s = repo.findByPlayerOrderByScoreDesc(player);
 		
+	}
+	//DELETE ALL SCORES
+	@DeleteMapping("/score")
+	public ResponseEntity<?> deleteAllScores(){
+		repo.deleteAll();
+		return ResponseEntity.status(200).body("DELETED!");
 	}
 	
 }
